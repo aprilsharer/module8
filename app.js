bcec8b67f2ebb27a98e2cd466e28d4c71e15649b
Isabella <digital.media.GitHub@gmail.com>
2026-03-05 08:29:02 -0600

    asharer
    emojis

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const circle = document.querySelector('#circle')
 circle.addEventListener('mouseenter', () => {
   if (!circle.classList.contains('hover')) {
     circle.classList.add('hover')
-    circle.textContent = 'BOO!'
+    circle.textContent = '🙉 BOO!'
   }
 })
 
 circle.addEventListener('mouseleave', () => {
   if (circle.classList.contains('hover')) {
     circle.classList.remove('hover')
-    circle.textContent = 'peek-a'
+    circle.textContent = '🙈 peek-a'
   }
 })
